8dd60f374c573ad7fdc69c72da32f4f80f5a409d
Matthew <MatthewJBelcher7@live.com>
2022-11-07 00:11:14 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,42 @@
 function addAnswers() {
-  let score1 = 0;
-  let score3 = 0;
-  let score3 = 0;
-  let score4 = 0;
+  var score1 = 0;
+  var score2 = 0;
+  var score3 = 0;
+  var score4 = 0;
 
-  let scores = document.getElementById("input");
+  var selection = document.getElementsByTagName('input');
 
-
-var selection = document.getElementsByClassName('input');
-
-for (i=0; i<selection.length; i++) {
-  if (selection[i].checked) {
-    if (selection[i].value == 'one') {
-      s1selection = s1selection + 1;
-    }
-    if (selection[i].value == 'two') {
-      s2selection = s2selection + 1;
-    }
-    if (selection[i].value == 'three') {
-      s3selection = s3selection + 1;
-    }
-    if (selection[i].value == 'four') {
-      s4selection = s4selection + 1;
+  for (i=0; i<selection.length; i++) {
+    if (selection[i].checked) {
+      if (selection[i].value == "one") {
+        score1 = score1 + 1;
+      }
+      if (selection[i].value == "two") {
+        score2 = score2 + 1;
+      }
+      if (selection[i].value == "three") {
+        score3 = score3 + 1;
+      }
+      if (selection[i].value == "four") {
+        score4 = score4 + 1;
+      }
     }
   }
-}
 
-var highestscore = Math.max(score1, score2, score3, score4);
+  var highestscore = Math.max(score1, score2, score3, score4);
 
-var results = document.getElementById('answer');
+  var results = document.getElementById('result');
 
-if (s1selection == highestscore) {
-  results.innerHTML = "You should learn Python!"
-}
-if (s2selection == highestscore) {
-  results.innerHTML = "You should learn C#!"
-}
-if (s3selection == highestscore) {
-  results.innerHTML = "You should learn Javascript!"
-}
-if (s4selection == highestscore) {
-  results.innerHTML = "You should learn Rust!"
-}
-
-fuction clearAwnser() {
-  var results = document.getElementById("answer");
-  results.innerHTML = "Awaiting Awnsers";
+  if (score1 == highestscore) {
+    results.innerHTML = "You should learn Python!"
+  }
+  if (score2 == highestscore) {
+    results.innerHTML = "You should learn C#!"
+  }
+  if (score3 == highestscore) {
+    results.innerHTML = "You should learn Javascript!"
+  }
+  if (score4 == highestscore) {
+    results.innerHTML = "You should learn Rust!"
+  }
 }
