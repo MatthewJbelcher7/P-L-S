0216ce2206519d89d3979e79501554f4c8360ba2
Matthew <MatthewJBelcher7@live.com>
2022-11-06 20:49:55 -0700

    add for js

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,26 @@ function addAnswers() {
   let scores = document.getElementById("input");
 }
 
+var selection = document.getElementsByClassName('input');
+
+for (i=0; i<selection.length; i++) {
+  if (selection[i].checked) {
+    if (selection[i].value == 's1') {
+      s1selection = s1selection + 1;
+    }
+    if (selection[i].value == 's2') {
+      s2selection = s2selection + 1;
+    }
+    if (selection[i].value == 's3') {
+      s3selection = s3selection + 1;
+    }
+    if (selection[i].value == 's4') {
+      s4selection = s4selection + 1;
+    }
+  }
+}
+
 fuction clearAwnser() {
   var answerbox = document.getElementById("answer");
-  awnserbox.innerHTML = "Awaiting Awnsers"
+  awnserbox.innerHTML = "Awaiting Awnsers";
 }
