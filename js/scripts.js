f67a6d7421aff83b38bf1c20aeda9874e4fb515b
Matthew <MatthewJBelcher7@live.com>
2022-11-06 23:17:11 -0700

    add results js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function addAnswers() {
   let score4 = 0;
 
   let scores = document.getElementById("input");
-}
+
 
 var selection = document.getElementsByClassName('input');
 
@@ -26,9 +26,24 @@ for (i=0; i<selection.length; i++) {
   }
 }
 
+var highestscore = Math.max(score1, score2, score3, score4);
 
+var results = document.getElementById('answer');
+
+if (s1selection == highestscore) {
+  results.innerHTML = "You should learn Python!"
+}
+if (s2selection == highestscore) {
+  results.innerHTML = "You should learn C#!"
+}
+if (s3selection == highestscore) {
+  results.innerHTML = "You should learn Javascript!"
+}
+if (s4selection == highestscore) {
+  results.innerHTML = "You should learn Rust!"
+}
 
 fuction clearAwnser() {
-  var answerbox = document.getElementById("answer");
-  awnserbox.innerHTML = "Awaiting Awnsers";
+  var results = document.getElementById("answer");
+  results.innerHTML = "Awaiting Awnsers";
 }
